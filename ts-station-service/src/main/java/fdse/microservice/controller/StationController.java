a31b5567ac6c587a5cadbcc73dc12582932889d6
lab401\lwh <2391289924@qq.com>
2018-01-04 15:59:20 +0800

    Fix the bug of ts-station-service: remove the unused import

--- ts-station-service/src/main/java/fdse/microservice/controller/StationController.java
+++ ts-station-service/src/main/java/fdse/microservice/controller/StationController.java
@@ -2,15 +2,9 @@ package fdse.microservice.controller;
 
 import fdse.microservice.domain.*;
 import fdse.microservice.service.StationService;
-import javafx.application.Application;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
