6d33acd1adc4e4e3d9704b93b557c0c41e6f3209
hechuan73 <1044856164@qq.com>
2018-01-15 23:02:40 +0800

    Description: rename the test case class.

--- tsp-test/TestServiceTicketOffice.java
+++ tsp-test/TestServiceTicketOffice.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class testServiceTicketOffice {
+public class TestServiceTicketOffice {
 
     private WebDriver driver;
     private String baseUrl;
