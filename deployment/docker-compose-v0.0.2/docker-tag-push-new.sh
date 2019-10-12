9ece180fdea3fc65066b0dd37c6eddeb7bb44a17
LeBW <27091925@qq.com>
2019-10-12 10:01:48 +0800

    modify quickstart-docker-compose.yml

---
+++ deployment/docker-compose-v0.0.2/docker-tag-push-new.sh
@@ -0,0 +1,83 @@
+docker tag ts/ts-train-service  libw521/ts-train-service
+docker tag ts/ts-ui-dashboard  libw521/ts-ui-dashboard
+docker tag ts/ts-auth-service  libw521/ts-auth-service
+docker tag ts/ts-user-service  libw521/ts-user-service 
+docker tag ts/ts-verification-code-service  libw521/ts-verification-code-service
+docker tag ts/ts-contacts-service libw521/ts-contacts-service
+docker tag ts/ts-order-service libw521/ts-order-service
+docker tag ts/ts-order-other-service libw521/ts-order-other-service
+docker tag ts/ts-config-service libw521/ts-config-service
+docker tag ts/ts-station-service  libw521/ts-station-service
+docker tag ts/ts-travel-service  libw521/ts-travel-service
+docker tag ts/ts-travel2-service  libw521/ts-travel2-service
+docker tag ts/ts-preserve-service  libw521/ts-preserve-service
+docker tag ts/ts-preserve-other-service   libw521/ts-preserve-other-service
+docker tag ts/ts-basic-service  libw521/ts-basic-service
+docker tag ts/ts-ticketinfo-service  libw521/ts-ticketinfo-service
+docker tag ts/ts-price-service  libw521/ts-price-service
+docker tag ts/ts-notification-service  libw521/ts-notification-service
+docker tag ts/ts-security-service  libw521/ts-security-service
+docker tag ts/ts-inside-payment-service  libw521/ts-inside-payment-service
+docker tag ts/ts-execute-service   libw521/ts-execute-service
+docker tag ts/ts-payment-service  libw521/ts-payment-service
+docker tag ts/ts-rebook-service  libw521/ts-rebook-service
+docker tag ts/ts-cancel-service  libw521/ts-cancel-service
+docker tag ts/ts-route-service  libw521/ts-route-service
+docker tag ts/ts-assurance-service  libw521/ts-assurance-service
+docker tag ts/ts-seat-service  libw521/ts-seat-service
+docker tag ts/ts-travel-plan-service  libw521/ts-travel-plan-service 
+docker tag ts/ts-route-plan-service  libw521/ts-route-plan-service 
+docker tag ts/ts-food-map-service   libw521/ts-food-map-service
+docker tag ts/ts-food-service  libw521/ts-food-service
+docker tag ts/ts-consign-price-service   libw521/ts-consign-price-service
+docker tag ts/ts-consign-service   libw521/ts-consign-service
+docker tag ts/ts-admin-order-service  libw521/ts-admin-order-service
+docker tag ts/ts-admin-basic-info-service  libw521/ts-admin-basic-info-service
+docker tag ts/ts-admin-route-service  libw521/ts-admin-route-service
+docker tag ts/ts-admin-travel-service  libw521/ts-admin-travel-service
+docker tag ts/ts-admin-user-service  libw521/ts-admin-user-service
+docker tag ts/ts-news-service  libw521/ts-news-service
+docker tag ts/ts-ticket-office-service  libw521/ts-ticket-office-service
+docker tag ts/ts-voucher-service  libw521/ts-voucher-service
+
+docker push libw521/ts-train-service
+docker push libw521/ts-ui-dashboard
+docker push libw521/ts-auth-service
+docker push libw521/ts-user-service
+docker push libw521/ts-verification-code-service
+docker push libw521/ts-contacts-service
+docker push libw521/ts-order-service
+docker push libw521/ts-order-other-service
+docker push libw521/ts-config-service
+docker push libw521/ts-station-service
+docker push libw521/ts-travel-service
+docker push libw521/ts-travel2-service
+docker push libw521/ts-preserve-service
+docker push libw521/ts-preserve-other-service
+docker push libw521/ts-basic-service
+docker push libw521/ts-ticketinfo-service
+docker push libw521/ts-price-service
+docker push libw521/ts-notification-service
+docker push libw521/ts-security-service
+docker push libw521/ts-inside-payment-service
+docker push libw521/ts-execute-service
+docker push libw521/ts-payment-service
+docker push libw521/ts-rebook-service
+docker push libw521/ts-cancel-service
+docker push libw521/ts-route-service
+docker push libw521/ts-assurance-service
+docker push libw521/ts-seat-service
+docker push libw521/ts-travel-plan-service
+docker push libw521/ts-route-plan-service
+docker push libw521/ts-food-map-service
+docker push libw521/ts-food-service
+docker push libw521/ts-consign-price-service
+docker push libw521/ts-consign-service
+docker push libw521/ts-admin-order-service
+docker push libw521/ts-admin-basic-info-service
+docker push libw521/ts-admin-route-service
+docker push libw521/ts-admin-travel-service
+docker push libw521/ts-admin-user-service
+docker push libw521/ts-news-service
+docker push libw521/ts-ticket-office-service
+docker push libw521/ts-voucher-service
