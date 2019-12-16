a68b506ed91d1ba97c37b2c4548f3282efe50024
LeBW <27091925@qq.com>
2019-12-16 07:00:26 -0500

    modify docker-tag-push.sh

---
+++ deployment/docker-compose-v0.0.2/docker-tag-push.sh
@@ -0,0 +1,83 @@
+docker tag ts/ts-train-service  codewisdom/ts-train-service:0.0.3
+docker tag ts/ts-ui-dashboard  codewisdom/ts-ui-dashboard:0.0.3
+docker tag ts/ts-auth-service  codewisdom/ts-auth-service:0.0.3
+docker tag ts/ts-user-service  codewisdom/ts-user-service:0.0.3
+docker tag ts/ts-verification-code-service  codewisdom/ts-verification-code-service:0.0.3
+docker tag ts/ts-contacts-service codewisdom/ts-contacts-service:0.0.3
+docker tag ts/ts-order-service codewisdom/ts-order-service:0.0.3
+docker tag ts/ts-order-other-service codewisdom/ts-order-other-service:0.0.3
+docker tag ts/ts-config-service codewisdom/ts-config-service:0.0.3
+docker tag ts/ts-station-service  codewisdom/ts-station-service:0.0.3
+docker tag ts/ts-travel-service  codewisdom/ts-travel-service:0.0.3
+docker tag ts/ts-travel2-service  codewisdom/ts-travel2-service:0.0.3
+docker tag ts/ts-preserve-service  codewisdom/ts-preserve-service:0.0.3
+docker tag ts/ts-preserve-other-service   codewisdom/ts-preserve-other-service:0.0.3
+docker tag ts/ts-basic-service  codewisdom/ts-basic-service:0.0.3
+docker tag ts/ts-ticketinfo-service  codewisdom/ts-ticketinfo-service:0.0.3
+docker tag ts/ts-price-service  codewisdom/ts-price-service:0.0.3
+docker tag ts/ts-notification-service  codewisdom/ts-notification-service:0.0.3
+docker tag ts/ts-security-service  codewisdom/ts-security-service:0.0.3
+docker tag ts/ts-inside-payment-service  codewisdom/ts-inside-payment-service:0.0.3
+docker tag ts/ts-execute-service   codewisdom/ts-execute-service:0.0.3
+docker tag ts/ts-payment-service  codewisdom/ts-payment-service:0.0.3
+docker tag ts/ts-rebook-service  codewisdom/ts-rebook-service:0.0.3
+docker tag ts/ts-cancel-service  codewisdom/ts-cancel-service:0.0.3
+docker tag ts/ts-route-service  codewisdom/ts-route-service:0.0.3
+docker tag ts/ts-assurance-service  codewisdom/ts-assurance-service:0.0.3
+docker tag ts/ts-seat-service  codewisdom/ts-seat-service:0.0.3
+docker tag ts/ts-travel-plan-service  codewisdom/ts-travel-plan-service:0.0.3
+docker tag ts/ts-route-plan-service  codewisdom/ts-route-plan-service:0.0.3 
+docker tag ts/ts-food-map-service   codewisdom/ts-food-map-service:0.0.3
+docker tag ts/ts-food-service  codewisdom/ts-food-service:0.0.3
+docker tag ts/ts-consign-price-service   codewisdom/ts-consign-price-service:0.0.3
+docker tag ts/ts-consign-service   codewisdom/ts-consign-service:0.0.3
+docker tag ts/ts-admin-order-service  codewisdom/ts-admin-order-service:0.0.3
+docker tag ts/ts-admin-basic-info-service  codewisdom/ts-admin-basic-info-service:0.0.3
+docker tag ts/ts-admin-route-service  codewisdom/ts-admin-route-service:0.0.3
+docker tag ts/ts-admin-travel-service  codewisdom/ts-admin-travel-service:0.0.3
+docker tag ts/ts-admin-user-service  codewisdom/ts-admin-user-service:0.0.3
+docker tag ts/ts-news-service  codewisdom/ts-news-service:0.0.3
+docker tag ts/ts-ticket-office-service  codewisdom/ts-ticket-office-service:0.0.3
+docker tag ts/ts-voucher-service  codewisdom/ts-voucher-service:0.0.3
+
+docker push codewisdom/ts-train-service:0.0.3
+docker push codewisdom/ts-ui-dashboard:0.0.3
+docker push codewisdom/ts-auth-service:0.0.3
+docker push codewisdom/ts-user-service:0.0.3
+docker push codewisdom/ts-verification-code-service:0.0.3
+docker push codewisdom/ts-contacts-service:0.0.3
+docker push codewisdom/ts-order-service:0.0.3
+docker push codewisdom/ts-order-other-service:0.0.3
+docker push codewisdom/ts-config-service:0.0.3
+docker push codewisdom/ts-station-service:0.0.3
+docker push codewisdom/ts-travel-service:0.0.3
+docker push codewisdom/ts-travel2-service:0.0.3
+docker push codewisdom/ts-preserve-service:0.0.3
+docker push codewisdom/ts-preserve-other-service:0.0.3
+docker push codewisdom/ts-basic-service:0.0.3
+docker push codewisdom/ts-ticketinfo-service:0.0.3
+docker push codewisdom/ts-price-service:0.0.3
+docker push codewisdom/ts-notification-service:0.0.3
+docker push codewisdom/ts-security-service:0.0.3
+docker push codewisdom/ts-inside-payment-service:0.0.3
+docker push codewisdom/ts-execute-service:0.0.3
+docker push codewisdom/ts-payment-service:0.0.3
+docker push codewisdom/ts-rebook-service:0.0.3
+docker push codewisdom/ts-cancel-service:0.0.3
+docker push codewisdom/ts-route-service:0.0.3
+docker push codewisdom/ts-assurance-service:0.0.3
+docker push codewisdom/ts-seat-service:0.0.3
+docker push codewisdom/ts-travel-plan-service:0.0.3
+docker push codewisdom/ts-route-plan-service:0.0.3
+docker push codewisdom/ts-food-map-service:0.0.3
+docker push codewisdom/ts-food-service:0.0.3
+docker push codewisdom/ts-consign-price-service:0.0.3
+docker push codewisdom/ts-consign-service:0.0.3
+docker push codewisdom/ts-admin-order-service:0.0.3
+docker push codewisdom/ts-admin-basic-info-service:0.0.3
+docker push codewisdom/ts-admin-route-service:0.0.3
+docker push codewisdom/ts-admin-travel-service:0.0.3
+docker push codewisdom/ts-admin-user-service:0.0.3
+docker push codewisdom/ts-news-service:0.0.3
+docker push codewisdom/ts-ticket-office-service:0.0.3
+docker push codewisdom/ts-voucher-service:0.0.3
