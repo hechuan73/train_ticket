1ffc4df996aec139d9f31c32b3dab335256a4338
zhongyuanzhao000 <zhongyuanzhao000@gmail.com>
2019-12-22 12:47:38 +0800

    add Jenkins CI

---
+++ jenkins-ci/image-tag-push.sh
@@ -0,0 +1,83 @@
+sudo docker tag ts/ts-train-service  codewisdom/ts-train-service:latest
+sudo docker tag ts/ts-ui-dashboard  codewisdom/ts-ui-dashboard:latest
+sudo docker tag ts/ts-auth-service  codewisdom/ts-auth-service:latest
+sudo docker tag ts/ts-user-service  codewisdom/ts-user-service:latest
+sudo docker tag ts/ts-verification-code-service  codewisdom/ts-verification-code-service:latest
+sudo docker tag ts/ts-contacts-service codewisdom/ts-contacts-service:latest
+sudo docker tag ts/ts-order-service codewisdom/ts-order-service:latest
+sudo docker tag ts/ts-order-other-service codewisdom/ts-order-other-service:latest
+sudo docker tag ts/ts-config-service codewisdom/ts-config-service:latest
+sudo docker tag ts/ts-station-service  codewisdom/ts-station-service:latest
+sudo docker tag ts/ts-travel-service  codewisdom/ts-travel-service:latest
+sudo docker tag ts/ts-travel2-service  codewisdom/ts-travel2-service:latest
+sudo docker tag ts/ts-preserve-service  codewisdom/ts-preserve-service:latest
+sudo docker tag ts/ts-preserve-other-service   codewisdom/ts-preserve-other-service:latest
+sudo docker tag ts/ts-basic-service  codewisdom/ts-basic-service:latest
+sudo docker tag ts/ts-ticketinfo-service  codewisdom/ts-ticketinfo-service:latest
+sudo docker tag ts/ts-price-service  codewisdom/ts-price-service:latest
+sudo docker tag ts/ts-notification-service  codewisdom/ts-notification-service:latest
+sudo docker tag ts/ts-security-service  codewisdom/ts-security-service:latest
+sudo docker tag ts/ts-inside-payment-service  codewisdom/ts-inside-payment-service:latest
+sudo docker tag ts/ts-execute-service   codewisdom/ts-execute-service:latest
+sudo docker tag ts/ts-payment-service  codewisdom/ts-payment-service:latest
+sudo docker tag ts/ts-rebook-service  codewisdom/ts-rebook-service:latest
+sudo docker tag ts/ts-cancel-service  codewisdom/ts-cancel-service:latest
+sudo docker tag ts/ts-route-service  codewisdom/ts-route-service:latest
+sudo docker tag ts/ts-assurance-service  codewisdom/ts-assurance-service:latest
+sudo docker tag ts/ts-seat-service  codewisdom/ts-seat-service:latest
+sudo docker tag ts/ts-travel-plan-service  codewisdom/ts-travel-plan-service:latest
+sudo docker tag ts/ts-route-plan-service  codewisdom/ts-route-plan-service:latest 
+sudo docker tag ts/ts-food-map-service   codewisdom/ts-food-map-service:latest
+sudo docker tag ts/ts-food-service  codewisdom/ts-food-service:latest
+sudo docker tag ts/ts-consign-price-service   codewisdom/ts-consign-price-service:latest
+sudo docker tag ts/ts-consign-service   codewisdom/ts-consign-service:latest
+sudo docker tag ts/ts-admin-order-service  codewisdom/ts-admin-order-service:latest
+sudo docker tag ts/ts-admin-basic-info-service  codewisdom/ts-admin-basic-info-service:latest
+sudo docker tag ts/ts-admin-route-service  codewisdom/ts-admin-route-service:latest
+sudo docker tag ts/ts-admin-travel-service  codewisdom/ts-admin-travel-service:latest
+sudo docker tag ts/ts-admin-user-service  codewisdom/ts-admin-user-service:latest
+sudo docker tag ts/ts-news-service  codewisdom/ts-news-service:latest
+sudo docker tag ts/ts-ticket-office-service  codewisdom/ts-ticket-office-service:latest
+sudo docker tag ts/ts-voucher-service  codewisdom/ts-voucher-service:latest
+
+sudo docker push codewisdom/ts-train-service:latest
+sudo docker push codewisdom/ts-ui-dashboard:latest
+sudo docker push codewisdom/ts-auth-service:latest
+sudo docker push codewisdom/ts-user-service:latest
+sudo docker push codewisdom/ts-verification-code-service:latest
+sudo docker push codewisdom/ts-contacts-service:latest
+sudo docker push codewisdom/ts-order-service:latest
+sudo docker push codewisdom/ts-order-other-service:latest
+sudo docker push codewisdom/ts-config-service:latest
+sudo docker push codewisdom/ts-station-service:latest
+sudo docker push codewisdom/ts-travel-service:latest
+sudo docker push codewisdom/ts-travel2-service:latest
+sudo docker push codewisdom/ts-preserve-service:latest
+sudo docker push codewisdom/ts-preserve-other-service:latest
+sudo docker push codewisdom/ts-basic-service:latest
+sudo docker push codewisdom/ts-ticketinfo-service:latest
+sudo docker push codewisdom/ts-price-service:latest
+sudo docker push codewisdom/ts-notification-service:latest
+sudo docker push codewisdom/ts-security-service:latest
+sudo docker push codewisdom/ts-inside-payment-service:latest
+sudo docker push codewisdom/ts-execute-service:latest
+sudo docker push codewisdom/ts-payment-service:latest
+sudo docker push codewisdom/ts-rebook-service:latest
+sudo docker push codewisdom/ts-cancel-service:latest
+sudo docker push codewisdom/ts-route-service:latest
+sudo docker push codewisdom/ts-assurance-service:latest
+sudo docker push codewisdom/ts-seat-service:latest
+sudo docker push codewisdom/ts-travel-plan-service:latest
+sudo docker push codewisdom/ts-route-plan-service:latest
+sudo docker push codewisdom/ts-food-map-service:latest
+sudo docker push codewisdom/ts-food-service:latest
+sudo docker push codewisdom/ts-consign-price-service:latest
+sudo docker push codewisdom/ts-consign-service:latest
+sudo docker push codewisdom/ts-admin-order-service:latest
+sudo docker push codewisdom/ts-admin-basic-info-service:latest
+sudo docker push codewisdom/ts-admin-route-service:latest
+sudo docker push codewisdom/ts-admin-travel-service:latest
+sudo docker push codewisdom/ts-admin-user-service:latest
+sudo docker push codewisdom/ts-news-service:latest
+sudo docker push codewisdom/ts-ticket-office-service:latest
+sudo docker push codewisdom/ts-voucher-service:latest
