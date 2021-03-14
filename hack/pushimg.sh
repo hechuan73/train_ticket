921a44015950512eada6cc6e49a7c972e630f5c2
Kagaya <kagaya85@outlook.com>
2021-03-14 15:12:09 +0800

    typo fix

--- hack/pushimg.sh
+++ hack/pushimg.sh
@@ -9,16 +9,16 @@ REPO=
 docker rmi "$(docker images -f "dangling=true" -q)"
 
 echo
-echo "Pleas input you repo password"
+echo "Please input you repo password"
 echo
 docker login --username="${USERNAME}" "${REPO}"
 
 echo
-echo "Start build image using docker-compose build"
+echo "Start building image using docker-compose build"
 echo
 docker-compose build
 
 echo
-echo "Start push image"
+echo "Start pushing image"
 echo
 docker images | grep "${NAMESPACE}/ts" | awk 'BEGIN{OFS=":"}{print $1,$2}' | xargs -I {} docker push {}
