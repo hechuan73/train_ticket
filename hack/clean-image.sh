b9030d158200ba28f6213323bae2d3a50f1ed53c
Kagaya <kagaya85@outlook.com>
2021-04-20 23:53:13 +0800

    fix image is referenced in multiple repositories

--- hack/clean-image.sh
+++ hack/clean-image.sh
@@ -3,7 +3,7 @@
 echo
 echo "Clean images, Repo: $1"
 echo
-images=$(docker images | grep "$1"/ts- | awk '{print $3}')
+images=$(docker images | grep "$1"/ts- | awk 'BEGIN{OFS=":"}{print $1,$2}')
 
 if [[ -n "$images" ]]; then
     echo "$images" | xargs -I {} docker rmi {}
